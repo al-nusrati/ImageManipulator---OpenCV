d5ae589f4497fca76122c76d2b7a623256ddc641
Jawad <Jawad@LAPTOP-UNDK30OF>
2025-10-19 12:58:27 +0500

    added image manipulator header file code

--- ImageManipulator - OpenCV/ImageManipulator.h
+++ ImageManipulator - OpenCV/ImageManipulator.h
@@ -1,5 +1,44 @@
-#pragma once
-class ImageManipulator
-{
-};
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <string>
+#include <iostream>
 
+class ImageManipulator {
+private:
+    cv::Mat originalImage;
+    cv::Mat processedImage;
+    std::string imagePath;
+
+public:
+    // Constructors
+    ImageManipulator();
+    ImageManipulator(const std::string& path);
+
+    // Destructor
+    ~ImageManipulator();
+
+    // Image Loading
+    bool loadImage(const std::string& path);
+
+    // Display methods
+    void displayOriginal() const;
+    void displayProcessed(const std::string& windowName = "Processed Image") const;
+
+    // Contrast Enhancement (Manual implementation - NO convertTo)
+    cv::Mat enhanceContrast(double alpha, double beta);
+
+    // Image Rotation
+    cv::Mat rotateImage(int angle);
+
+    // Image Flipping
+    cv::Mat flipImage(int flipCode);
+
+    // Utility methods
+    cv::Mat getOriginalImage() const;
+    cv::Mat getProcessedImage() const;
+    bool isImageLoaded() const;
+
+    // Save processed image
+    bool saveImage(const std::string& outputPath) const;
+};
